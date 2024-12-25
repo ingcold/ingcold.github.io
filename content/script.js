949c48038310b549d4b68c5dcaa4f227145a88e0
ingcold <2927709232@qq.com>
2024-12-25 18:14:53 +0800

    12/25上传

--- content/script.js
+++ content/script.js
@@ -3,21 +3,29 @@ const open_btn = document.querySelector('.open-btn')
 const close_btn = document.querySelector('.close-btn')
 const nav = document.querySelectorAll('.nav')
 
+this.count=0
 var autoOpen=true
-function autoOpenSetBool() {
-    autoOpen=false
-}
+var after="after"
 
-if(autoOpen){
+
+if(after=="after"){
 nav.forEach(nav_el => nav_el.classList.add('visible'))
 }
 
 open_btn.addEventListener('click', () => {
     nav.forEach(nav_el => nav_el.classList.add('visible'))
+    after="after"
 })
 
 close_btn.addEventListener('click', () => {
     nav.forEach(nav_el => nav_el.classList.remove('visible'))
+    after="after"
 })
 
-document.getElementById('label').innerHTML = autoOpen?'自动打开左侧栏':'自动关闭左侧栏'
+
+
+function toggleSubmenu(submenuId) {
+    const submenu = document.getElementById(submenuId);
+    submenu.style.display = (submenu.style.display === 'block') ? 'none' : 'block';
+}
+
